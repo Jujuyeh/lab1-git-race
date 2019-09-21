dce5a4e785c4cf3f9f802a9861d2922bd5ddf6e2
unknown <diego.roymen@gmail.com>
2019-09-21 14:18:33 +0200

    Last Visit unit test
    Checks if status is OK and if attribute exists

---
+++ src/test/java/es/unizar/webeng/hello/LastVisitUnitTest.java
@@ -0,0 +1,40 @@
+package es.unizar.webeng.hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(LastVisit.class)
+public class LastVisitUnitTest {
+
+    @MockBean
+    private ValueOperations<String, String> valueOperations;
+
+    @MockBean
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testLastVisit() throws Exception {
+	given(stringRedisTemplate.opsForValue()).willReturn(valueOperations);
+        this.mvc.perform(get("/visit")).
+                andExpect(status().isOk()).
+                andExpect(model().attributeExists("displayText"));
+    }
+
+}
