7a722e6c72c26fe8efc0cfe32e22485b1ac24a12
DanFzgz <fraile.daniel@yahoo.es>
2019-09-22 19:05:14 +0200

    Added brief information about the last commit of the repository

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -22,15 +22,19 @@ import java.net.UnknownHostException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.apache.commons.lang3.StringUtils;
 
 @Controller
 public class HelloController {
-    
+
     Logger logger = LoggerFactory.getLogger(HelloController.class);
-    
+
     /**
-     * Sets message value from properties,
-     * or default string if not defined.
+     * Sets message value from properties, or default string if not defined.
      */
     @Value("${app.message:Hello World}")
     private String message;
@@ -40,30 +44,71 @@ public class HelloController {
 
     @Value("${app.visitorCount}")
     private int visitorCount;
-    
+
     @Autowired
     private HttpServletRequest request;
 
-    private long getSecondsLeft(long difference){
-        return difference/1000%60;
+    private long getSecondsLeft(long difference) {
+        return difference / 1000 % 60;
     }
 
-    private long getMinutesLeft(long difference){
-        return difference/(1000*60)%60;
+    private long getMinutesLeft(long difference) {
+        return difference / (1000 * 60) % 60;
     }
 
-    private long getHoursLeft(long difference){
-        return difference/(1000*60*60)%24;
+    private long getHoursLeft(long difference) {
+        return difference / (1000 * 60 * 60) % 24;
     }
 
-    private long getDaysLeft(long difference){
-        return difference/(1000*60*60*24);
+    private long getDaysLeft(long difference) {
+        return difference / (1000 * 60 * 60 * 24);
+    }
+
+    private void getGitHubInfo(Map<String, Object> model) {
+        try {
+            URL repo = new URL("https://api.github.com/repos/UNIZAR-30246-WebEngineering/lab1-git-race/commits");
+            HttpURLConnection conection = (HttpURLConnection) repo.openConnection();
+            int status = conection.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+            String inputLine = in.readLine();
+            in.close();
+            if(inputLine != null) {
+                if(inputLine.contains("date")){
+                    String date = StringUtils.substringBetween(inputLine, "\"date\":\"", "\"");
+                    model.put("commitDate", date);
+                }
+                else {
+                    model.put("commitDate", "-");
+                }
+                if(inputLine.contains("message")){
+                    String commitmessage = StringUtils.substringBetween(inputLine, "\"message\":\"", "\"");
+                    System.out.println(commitmessage.replace("\\n", "-"));
+                    model.put("commitMessage", commitmessage.replace("\\n", "-"));
+                }
+                else {
+                    model.put("commitMessage", "-");
+                }
+            }
+            else {
+                model.put("commitDate", "-");
+                model.put("commitMessage", "-");
+            }
+        /**
+         * There is a rate limit of up to 60 requests per hour, an exception is rose
+         * in case this limit is exceeded.
+         * @see https://developer.github.com/v3/#rate-limiting
+         */
+        } catch (Exception e) {
+            model.put("commitDate", "-");
+            model.put("commitMessage", "-");
+            logger.debug("GitHub related error");
+        }
     }
 
     /**
      * Application welcome page.
      *
-     * @param model  the attributes for rendering, not null
+     * @param model the attributes for rendering, not null
      * @return the view name
      */
     @GetMapping("/")
@@ -71,16 +116,15 @@ public class HelloController {
         /** Sets "os" and "version" from system information */
         model.put("os", System.getProperty("os.name"));
         model.put("version", System.getProperty("os.version"));
-        /** @see https://stackoverflow.com/questions/7883542/getting-the-computer-name-in-java */
-        try
-        {
+        /**
+         * @see https://stackoverflow.com/questions/7883542/getting-the-computer-name-in-java
+         */
+        try {
             InetAddress addr;
             addr = InetAddress.getLocalHost();
             /** Sets "hostname" attribute to server machine name */
             model.put("hostname", addr.getHostName());
-        }
-        catch (UnknownHostException ex)
-        {
+        } catch (UnknownHostException ex) {
             logger.info("Hostname can not be resolved");
         }
         /** Sets "time" attribute to current date and time */
@@ -91,7 +135,9 @@ public class HelloController {
         /** Sets IP of last request */
         model.put("last_ip", last_ip);
         try {
-            /** @see https://www.mkyong.com/spring-mvc/spring-mvc-how-to-get-client-ip-address/ */
+            /**
+             * @see https://www.mkyong.com/spring-mvc/spring-mvc-how-to-get-client-ip-address/
+             */
             String remoteAddr = "";
             if (request != null) {
                 remoteAddr = request.getHeader("X-FORWARDED-FOR");
@@ -100,15 +146,14 @@ public class HelloController {
                 }
             }
             last_ip = remoteAddr;
-        }
-        catch (IllegalStateException e) {
+        } catch (IllegalStateException e) {
             /** For unit tests */
             logger.debug("Request object is not valid");
         }
         model.put("message", message);
 
         /* It adds one to the visits of the web */
-        synchronized(this){ // We do it in an atomic way to avoid race conditions.
+        synchronized (this) { // We do it in an atomic way to avoid race conditions.
             visitorCount++;
         }
         model.put("visitorCount", visitorCount);
@@ -125,6 +170,7 @@ public class HelloController {
         /** Sets days left to deadline */
         model.put("daysLeft", getDaysLeft(countdownDifference));
         /** Renders "wellcome" view using "model" attributes */
+        getGitHubInfo(model);
         return "wellcome";
     }
 
--- src/main/webapp/WEB-INF/jsp/wellcome.jsp
+++ src/main/webapp/WEB-INF/jsp/wellcome.jsp
@@ -29,7 +29,9 @@ ${daysLeft} days, ${hoursLeft} hours, ${minutesLeft} minutes and ${secondsLeft} 
 <br>
 Do you wanna know if you would survive thanos snap?? ->> <a href="./thanos">CLICK HERE</a><br>
 <br>
-user@${hostname}:~$ ${message}</kbd>
+user@${hostname}:~$ ${message}<br>
+<br>
+Last commit(${commitDate}): ${commitMessage}</kbd>
 <%-- Links to JQuery JavaScript library as a webjar dependency --%>
 <script type="text/javascript" src="webjars/jquery/2.1.4/jquery.min.js"></script>
 </body>
