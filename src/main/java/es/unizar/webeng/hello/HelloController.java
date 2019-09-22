a913bc396cd04e098b4777d8a3d2d04f8511b2fb
aeri <navaldesu@yahoo.co.jp>
2019-09-22 17:00:47 +0200

    Code reformatted and code encapsulted

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -24,95 +24,103 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class HelloController {
 
-	Logger logger = LoggerFactory.getLogger(HelloController.class);
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-	/**
-	 * Sets message value from properties, or default string if not defined.
-	 */
-	@Value("${app.message:Hello World}")
-	private String message;
-	private Date last_time = new Date();
-	private String last_ip = "0.0.0.0";
-	private String userAgent;
+    /**
+     * Sets message value from properties, or default string if not defined.
+     */
+    @Value("${app.message:Hello World}")
+    private String message;
+    private Date last_time = new Date();
+    private String last_ip = "0.0.0.0";
+    private String userAgent;
 
-	@Autowired
-	private HttpServletRequest request;
+    @Autowired
+    private HttpServletRequest request;
 
-	/**
-	 * Application welcome page.
-	 *
-	 * @param model the attributes for rendering, not null
-	 * @return the view name
-	 */
-	@GetMapping("/")
-	public String welcome(HttpServletResponse response, Map<String, Object> model,
-			@CookieValue(value = "yay", required = false) String yay) {
-		/** Sets "os" and "version" from system information */
-		model.put("os", System.getProperty("os.name"));
-		model.put("version", System.getProperty("os.version"));
-		/**
-		 * @see https://stackoverflow.com/questions/7883542/getting-the-computer-name-in-java
-		 */
-		try {
-			InetAddress addr;
-			addr = InetAddress.getLocalHost();
-			/** Sets "hostname" attribute to server machine name */
-			model.put("hostname", addr.getHostName());
-		} catch (UnknownHostException ex) {
-			logger.info("Hostname can not be resolved");
-		}
-		/** Sets "time" attribute to current date and time */
-		model.put("time", new Date());
-		/** Sets time of last request */
-		model.put("last_time", last_time);
-		last_time = new Date();
-		/** Sets IP of last request */
-		model.put("last_ip", last_ip);
-		try {
-			/**
-			 * @see https://www.mkyong.com/spring-mvc/spring-mvc-how-to-get-client-ip-address/
-			 */
-			String remoteAddr = "";
-			if (request != null) {
-				remoteAddr = request.getHeader("X-FORWARDED-FOR");
-				if (remoteAddr == null || "".equals(remoteAddr)) {
-					remoteAddr = request.getRemoteAddr();
-				}
-			}
-			last_ip = remoteAddr;
-		} catch (IllegalStateException e) {
-			/** For unit tests */
-			logger.debug("Request object is not valid");
-		}
-		model.put("message", message);
-		/** Renders "wellcome" view using "model" attributes */
+    /**
+     * Application welcome page.
+     *
+     * @param model the attributes for rendering, not null
+     * @return the view name
+     */
 
-		try {
-			if (request != null) {
-				userAgent = request.getHeader("User-Agent");
-				model.put("uagent", userAgent);
-			}
-		} catch (IllegalStateException e) {
-			/** For unit tests */
-			logger.debug("Request object is not valid");
-		}
+    //Function to track the user
+    void tracking(HttpServletResponse response, Map<String, Object> model, String yay) {
 
-		if (yay == null) {
-			model.put("color", "red");
-			model.put("status", "New user");
-			// create a cookie
-			Cookie cookie = new Cookie("yay", "Welcome");
+        try {
+            if (request != null) {
+                userAgent = request.getHeader("User-Agent");
+                model.put("uagent", userAgent);
+            }
+        } catch (IllegalStateException e) {
+            /** For unit tests */
+            logger.debug("Request object is not valid");
+        }
 
-			// add cookie to response
-			response.addCookie(cookie);
-		} else {
-			model.put("color", "green");
-			model.put("status", "Previous user");
+        if (yay == null) {
+            model.put("color", "red");
+            model.put("status", "New user");
+            // create a cookie
+            Cookie cookie = new Cookie("yay", "Welcome");
 
-		}
+            // add cookie to response
+            response.addCookie(cookie);
+        } else {
+            model.put("color", "green");
+            model.put("status", "Previous user");
 
-		return "wellcome";
+        }
 
-	}
+    }
+
+    @GetMapping("/")
+    public String welcome(HttpServletResponse response, Map<String, Object> model,
+            @CookieValue(value = "yay", required = false) String yay) {
+        /** Sets "os" and "version" from system information */
+        model.put("os", System.getProperty("os.name"));
+        model.put("version", System.getProperty("os.version"));
+        /**
+         * @see https://stackoverflow.com/questions/7883542/getting-the-computer-name-in-java
+         */
+        try {
+            InetAddress addr;
+            addr = InetAddress.getLocalHost();
+            /** Sets "hostname" attribute to server machine name */
+            model.put("hostname", addr.getHostName());
+        } catch (UnknownHostException ex) {
+            logger.info("Hostname can not be resolved");
+        }
+        /** Sets "time" attribute to current date and time */
+        model.put("time", new Date());
+        /** Sets time of last request */
+        model.put("last_time", last_time);
+        last_time = new Date();
+        /** Sets IP of last request */
+        model.put("last_ip", last_ip);
+        try {
+            /**
+             * @see https://www.mkyong.com/spring-mvc/spring-mvc-how-to-get-client-ip-address/
+             */
+            String remoteAddr = "";
+            if (request != null) {
+                remoteAddr = request.getHeader("X-FORWARDED-FOR");
+                if (remoteAddr == null || "".equals(remoteAddr)) {
+                    remoteAddr = request.getRemoteAddr();
+                }
+            }
+            last_ip = remoteAddr;
+        } catch (IllegalStateException e) {
+            /** For unit tests */
+            logger.debug("Request object is not valid");
+        }
+        model.put("message", message);
+        /** Renders "wellcome" view using "model" attributes */
+
+        tracking(response, model, yay);
+
+        return "wellcome";
+
+    }
 
 }
--- src/test/java/es/unizar/webeng/hello/HelloControllerUnitTest.java
+++ src/test/java/es/unizar/webeng/hello/HelloControllerUnitTest.java
@@ -19,80 +19,76 @@ import static org.junit.Assert.assertNotNull;
 @WebMvcTest(HelloController.class)
 public class HelloControllerUnitTest {
 
-	/**
-	 * Sets message value from properties or default string if not defined
-	 */
-	@Value("${app.message:Hello World}")
-	private String message;
+    /**
+     * Sets message value from properties or default string if not defined
+     */
+    @Value("${app.message:Hello World}")
+    private String message;
 
-	@Autowired
-	private HelloController controller;
+    @Autowired
+    private HelloController controller;
 
-	/**
-	 * Checks message to be displayed
-	 */
-	@Test
-	public void testMessage() throws Exception {
-		HashMap<String, Object> map = new HashMap<>();
-		String cok = new String();
-		String view = controller.welcome(null, map, cok);
-		/** Checks if controller uses "wellcome" view */
-		assertThat(view, is("wellcome"));
-		/** Checks if message exists */
-		assertThat(map.containsKey("message"), is(true));
-		/** Checks if message matches expected value */
-		assertThat(map.get("message"), is(message));
-	}
+    /**
+     * Checks message to be displayed
+     */
+    @Test
+    public void testMessage() throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        String view = controller.welcome(null, map, "");
+        /** Checks if controller uses "wellcome" view */
+        assertThat(view, is("wellcome"));
+        /** Checks if message exists */
+        assertThat(map.containsKey("message"), is(true));
+        /** Checks if message matches expected value */
+        assertThat(map.get("message"), is(message));
+    }
 
-	/**
-	 * Checks time to be displayed
-	 */
-	@Test
-	public void testTime() throws Exception {
-		HashMap<String, Object> map = new HashMap<>();
-		String cok = new String();
-		String view = controller.welcome(null, map, cok);
-		/** Checks if controller uses "wellcome" view */
-		assertThat(view, is("wellcome"));
-		/** Checks if time exists */
-		assertThat(map.containsKey("time"), is(true));
-		/** Checks if time is before current time */
-		assertThat((Date) map.get("time"), lessThanOrEqualTo(new Date()));
-	}
+    /**
+     * Checks time to be displayed
+     */
+    @Test
+    public void testTime() throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        String view = controller.welcome(null, map, "");
+        /** Checks if controller uses "wellcome" view */
+        assertThat(view, is("wellcome"));
+        /** Checks if time exists */
+        assertThat(map.containsKey("time"), is(true));
+        /** Checks if time is before current time */
+        assertThat((Date) map.get("time"), lessThanOrEqualTo(new Date()));
+    }
 
-	/**
-	 * Checks server system information
-	 */
-	@Test
-	public void testSystem() throws Exception {
-		HashMap<String, Object> map = new HashMap<>();
-		String cok = new String();
-		String view = controller.welcome(null, map, cok);
-		/** Checks if controller uses "wellcome" view */
-		assertThat(view, is("wellcome"));
-		/** Checks if data exists and is not null */
-		assertThat(map.containsKey("os"), is(true));
-		assertNotNull(map.get("os"));
-		assertThat(map.containsKey("version"), is(true));
-		assertNotNull(map.get("version"));
-		assertThat(map.containsKey("hostname"), is(true));
-		assertNotNull(map.get("hostname"));
-	}
+    /**
+     * Checks server system information
+     */
+    @Test
+    public void testSystem() throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        String view = controller.welcome(null, map, "");
+        /** Checks if controller uses "wellcome" view */
+        assertThat(view, is("wellcome"));
+        /** Checks if data exists and is not null */
+        assertThat(map.containsKey("os"), is(true));
+        assertNotNull(map.get("os"));
+        assertThat(map.containsKey("version"), is(true));
+        assertNotNull(map.get("version"));
+        assertThat(map.containsKey("hostname"), is(true));
+        assertNotNull(map.get("hostname"));
+    }
 
-	/**
-	 * Checks client information
-	 */
-	@Test
-	public void testClient() throws Exception {
-		HashMap<String, Object> map = new HashMap<>();
-		String cok = new String();
-		String view = controller.welcome(null, map, cok);
-		/** Checks if controller uses "wellcome" view */
-		assertThat(view, is("wellcome"));
-		/** Checks if data exists and is valid */
-		assertThat(map.containsKey("last_ip"), is(true));
-		assertNotNull(map.get("last_ip"));
-		assertThat(map.containsKey("last_time"), is(true));
-		assertThat((Date) map.get("last_time"), lessThanOrEqualTo(new Date()));
-	}
+    /**
+     * Checks client information
+     */
+    @Test
+    public void testClient() throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        String view = controller.welcome(null, map, "");
+        /** Checks if controller uses "wellcome" view */
+        assertThat(view, is("wellcome"));
+        /** Checks if data exists and is valid */
+        assertThat(map.containsKey("last_ip"), is(true));
+        assertNotNull(map.get("last_ip"));
+        assertThat(map.containsKey("last_time"), is(true));
+        assertThat((Date) map.get("last_time"), lessThanOrEqualTo(new Date()));
+    }
 }
