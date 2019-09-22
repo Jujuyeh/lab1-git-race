55dc2a93789bedca1e8f12d7729b05c38de7c179
jorge <jorge97fernandez@gmail.com>
2019-09-22 19:21:20 +0200

    Added Swagger Documentation Configuration

--- src/main/java/es/unizar/webeng/hello/Application.java
+++ src/main/java/es/unizar/webeng/hello/Application.java
@@ -5,16 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//Imports for use of Swagger
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.Bean;
-
 @SpringBootApplication
-@EnableSwagger2
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -24,13 +15,5 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)  
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build();
     }
 }
